f3d97c32ede520dc9060e12cd9d11911399190a7
Elena Nikiforova <vizualnoe@gmail.com>
2023-10-27 14:32:45 +0200

    add react hook form and loader

--- app/components/screens/auth/Auth.tsx
+++ app/components/screens/auth/Auth.tsx
@@ -1,11 +1,32 @@
-import { FC } from 'react'
+import Loader from '@/components/ui/Loader'
+import { IAuthFormData } from '@/shared/types/auth.interface'
+import { FC, useState } from 'react'
+import { useForm, SubmitHandler} from 'react-hook-form'
+import { Text, View } from 'react-native'
 
 const Auth: FC = () => {
+  const [isReg, setIsReg] = useState(false)
+
+  const {handleSubmit, control, reset} = useForm<IAuthFormData>({
+    mode: 'onChange',
+  })
+
+  const onSubmit:SubmitHandler<IAuthFormData> = ({email, password}) => {
+    
+  }
+
+  const isLoading = false
 
 	return (
-		<>
-			
-		</>
+		<View className='mx-2 items-center justify-center h-full'>
+      <View className="w-9/12">
+        <Text className='text-center text-white text-4xl font-bold mb-2.5'>
+          {isReg ? 'Register' : 'Login'}
+        </Text>
+
+        {isLoading ? <Loader /> : null}
+      </View>
+		</View>
 	)
 }
 
