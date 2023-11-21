94d2a150ccd92e0cd55aaa674606d54d07408444
Elena Nikiforova <vizualnoe@gmail.com>
2023-11-21 10:00:34 +0100

    finish MovieItem component

--- app/components/ui/movie/movie-item/MovieItem.tsx
+++ app/components/ui/movie/movie-item/MovieItem.tsx
@@ -1,24 +1,78 @@
 import { useTypedNavigation } from '@/hooks/useTypedNavigation'
 import { useTypedRoute } from '@/hooks/useTypedRoute'
 import { FC } from 'react'
-import { Pressable, Text, View } from 'react-native'
+import { Image, Pressable, Text, View, StyleSheet } from 'react-native'
 import Animated from 'react-native-reanimated'
+import { useMovieItemAnimation } from './useMovieItemAnimation'
+import { IMovie } from '@/shared/types/movie.interface'
+import FavoriteButton from './favorite-button/FavoriteButton'
+import { getMediaSource } from '@/utils/getMediaSource'
+import { BlurView } from 'expo-blur'
+import Rating from './Rating'
 
 const ReanimatedPressable = Animated.createAnimatedComponent(Pressable)
 
-const MovieItem: FC = () => {
+interface IMovieItem {
+  index: number
+  className?: string
+  movie: IMovie
+}
+
+const MovieItem: FC<IMovieItem> = ({index, movie}) => {
 
   const {navigate} = useTypedNavigation()
-
   const {name} = useTypedRoute()
 
   const isFavoritePage = name === 'Favorites'
 
-  /*Animations*/
+  const {styleAnimation} = useMovieItemAnimation(index)
 
 	return (
-		<ReanimatedPressable>
-			<Text>MovieItem</Text>
+		<ReanimatedPressable 
+      style={styleAnimation}
+      onPress={() => 
+        navigate('Movie', {
+          slug: movie.slug,
+        })
+      }
+      className={'rounded-xl overflow-hidden h-56'}
+    >
+
+      {isFavoritePage && (
+        <View className='absolute z-1 right-1.5 top-1.5'>
+          <FavoriteButton 
+            movieId={movie._id}
+            isSmall
+          />
+        </View>
+      )}
+
+      <Image
+				style={{
+					resizeMode: 'cover',
+					...StyleSheet.absoluteFillObject
+				}}
+				source={getMediaSource(movie.poster)}
+			/>
+
+      <BlurView
+        intensity={25}
+				className={
+					'absolute w-full bottom-0 left-0 right-0 items-center pt-0.5 px-2'
+				}
+      >
+        <View className='-ml-2 -mb-0.5'>
+          <Rating rating={movie.rating} size={16}/>
+        </View>
+
+        <Text
+					className='text-white text-lg font-semibold mb-1'
+					numberOfLines={1}
+				>
+					{movie.title}
+				</Text>
+      </BlurView>
+
 		</ReanimatedPressable>
 	)
 }
